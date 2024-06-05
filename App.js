eba0df557b4e92a8806c2e775c51252fe4930320
uedoca <mariaeduardaueda10@gmail.com>
2024-06-05 14:40:52 -0300

    ok

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { ThemeProvider } from "./src/contexts/ThemeContext";
+import AppNavigator from "./src/navigation/AppNavigator";
 
 
 export default function App() {
   return (
     <ThemeProvider>
-      
+      <AppNavigator />
     </ThemeProvider>
   );
 }
